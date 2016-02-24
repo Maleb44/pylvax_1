fbcc692f97ce435a5a77a55ce77f8fcc06654931
Zsolt Ero <zsolt.ero@gmail.com>
2016-02-24 19:03:04 +0100

    bottle 1, 3

--- http_server/bottle1.py
+++ http_server/bottle1.py
@@ -3,16 +3,30 @@ from bottle import route, run
 
 @route('/hello')
 @route('/hello/<name>')
-def hello(name='valaki'):
+@route('/hello/<name>/<width>')
+def hello(name='valaki', width=300):
     name_title = name.title()
 
     output_template = 'Hello {img}{name}!'
 
     if name == 'eniko':
-        img = '<img src="http://polomania.hu/images/designs/tn3/1987904494512cb7497ee46.jpg">'
+        img = '<img src="http://polomania.hu/images/designs/tn3/1987904494512cb7497ee46.jpg" width={width}>'\
+            .format(width=width)
     else:
         img = ''
 
     return output_template.format(img=img, name=name_title)
 
 run(host='localhost', port=8080, debug=True)
+
+
+@route('/sq/<number>')
+def sq(number):
+    number = float(number)
+    return str(number ** 2)
+
+
+@route('/palindrom/<word>')
+def is_palindrom(word):
+    result = word == word[::-1]
+    return str(result)
---
+++ http_server/bottle3.py
@@ -0,0 +1,33 @@
+from bottle import route, run
+
+import requests
+
+url_template = 'https://api.forecast.io/forecast/{api}/{lat},{lon}'
+apikey = 'e2fd60c047ae3fac95a0618a98a9e5fd'
+
+
+def get_temperature_lat_lon(lat, lon):
+    url = url_template.format(api=apikey, lat=lat, lon=lon)
+
+    params_dict = {
+        'units': 'si'
+    }
+
+    r = requests.get(url, params=params_dict)
+    if not r.ok:
+        return 'error'
+
+    data = r.json()
+    temp = data['currently']['temperature']
+    return temp
+
+
+@route('/temperature/<lat>/<lon>')
+def temp_endpoint_lat_lon(lat, lon):
+    lat = float(lat)
+    lon = float(lon)
+    temp = get_temperature_lat_lon(lat, lon)
+    return str(temp)
+
+
+run(host='localhost', port=8080, debug=True)
